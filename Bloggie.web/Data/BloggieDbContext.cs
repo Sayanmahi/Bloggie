a839f94f229d01c768a2f5c323caebaadf3028dd
Sayanmahi <sayanmukherjeedec812@gmail.com>
2023-10-19 20:45:34 +0530

    Add project files.

---
+++ Bloggie.web/Data/BloggieDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Bloggie.web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.web.Data
+{
+    public class BloggieDbContext:DbContext
+    {
+        public BloggieDbContext(DbContextOptions options): base(options)
+        {
+            
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tag { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Bloggie;");
+        }
+    }
+}
