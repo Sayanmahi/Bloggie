1bbef028d42b8adedbc476a30f41a677cbf9e7d2
Sayanmahi <sayanmukherjeedec812@gmail.com>
2023-10-23 20:35:39 +0530

    one way to get data from forms to backend.

--- Bloggie.web/Pages/Admin/Blogs/Add.cshtml.cs
+++ Bloggie.web/Pages/Admin/Blogs/Add.cshtml.cs
@@ -5,7 +5,24 @@ namespace Bloggie.web.Pages.Admin.Blogs
 {
     public class AddModel : PageModel
     {
+        [BindProperty]
+        public string Heading { get; set; }
+        [BindProperty]
+        public string PageTitle { get; set; }
+        [BindProperty]
+        public string Content { get; set; }
+        [BindProperty]
+        public string ShortDescription { get; set; }
+        [BindProperty]
+        public string FeaturedImageUrl { get; set; }
+        [BindProperty]
+        public string PublishedDate { get; set; }
+        [BindProperty]
+        public string Author { get; set; }
         public void OnGet()
+        {
+        }
+        public void OnPost()
         {
         }
     }
