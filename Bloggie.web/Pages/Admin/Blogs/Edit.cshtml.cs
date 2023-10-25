a6fb18c2f9d91939e2cbb7ee7cffc205cd5b18af
Sayanmahi <sayanmukherjeedec812@gmail.com>
2023-10-25 19:16:19 +0530

    Edit Part for Blogs done

---
+++ Bloggie.web/Pages/Admin/Blogs/Edit.cshtml.cs
@@ -0,0 +1,41 @@
+using Bloggie.web.Data;
+using Bloggie.web.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bloggie.web.Pages.Admin.Blogs
+{
+    public class EditModel : PageModel
+    {
+        private readonly BloggieDbContext db;
+        [BindProperty]
+        public BlogPost BlogPost { get; set; }
+        public EditModel(BloggieDbContext bl)
+        {
+            db = bl;
+        }
+        public void OnGet(Guid Id)
+        {
+            BlogPost = db.BlogPosts.Find(Id);
+        }
+        public IActionResult OnPost()
+        {
+            var ex=db.BlogPosts.Find(BlogPost.Id);
+            if (ex != null)
+            {
+                ex.Heading = BlogPost.Heading;
+                ex.PageTitle = BlogPost.PageTitle;
+                ex.Content = BlogPost.Content;
+                ex.ShortDescription = BlogPost.ShortDescription;
+                ex.FeaturedImageUrl= BlogPost.FeaturedImageUrl;
+                ex.UrlHandle= BlogPost.UrlHandle;
+                ex.PublishedDate= BlogPost.PublishedDate;
+                ex.Author= BlogPost.Author;
+                ex.Visible= BlogPost.Visible;
+            }
+            db.SaveChanges();
+            return RedirectToPage("/Admin/Blogs/List");
+
+        }
+    }
+}
