0f104887770908ddc73713fbea6e259f91cf47bf
Sayanmahi <sayanmukherjeedec812@gmail.com>
2023-11-07 19:34:42 +0530

    linking of blogpost model with tag with one blogpost to many tags

---
+++ Bloggie.web/Migrations/20231107140331_adding tags navigation property.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bloggie.web.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingtagsnavigationproperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Tag_BlogPostId",
+                table: "Tag",
+                column: "BlogPostId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tag_BlogPosts_BlogPostId",
+                table: "Tag",
+                column: "BlogPostId",
+                principalTable: "BlogPosts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tag_BlogPosts_BlogPostId",
+                table: "Tag");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tag_BlogPostId",
+                table: "Tag");
+        }
+    }
+}
