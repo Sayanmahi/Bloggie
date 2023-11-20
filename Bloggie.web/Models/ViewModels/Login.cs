f3435d6b4c77af9de03981ebe3a4ab2136eaad6f
Sayanmahi <sayanmukherjeedec812@gmail.com>
2023-11-20 18:33:35 +0530

    l

---
+++ Bloggie.web/Models/ViewModels/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace Bloggie.web.Models.ViewModels
+{
+    public class Login
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }    
+    }
+}
---
+++ Bloggie.web/Pages/Login.cshtml.cs
@@ -0,0 +1,39 @@
+using Bloggie.web.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bloggie.web.Pages
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Login LoginViewModel{get;set;}
+        public SignInManager<IdentityUser> signInManager { get; }
+
+        public LoginModel(SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+        public void OnGet()
+        {
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            var signInResult=await signInManager.PasswordSignInAsync(LoginViewModel.UserName,LoginViewModel.Password,false,false);
+            if(signInResult.Succeeded) 
+            {
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                ViewData["Notification"] = new Notification
+                {
+                    Type = Enums.NotificationType.Error,
+                    Message = "Wrong Credentials!"
+                };
+                return Page();
+            }
+        }
+    }
+}
